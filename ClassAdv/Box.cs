12dd18d7142c810002477fbbb7aa21b8bf081e99
xxx-Event <s2119@e-mirim.hs.kr>
2023-06-15 09:52:36 +0900

    속성(Property) 실습

---
+++ ClassAdv/Box.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Dynamic;
+
+namespace ClassAdv32
+{
+    internal class Box
+    {
+        private int width;
+
+        public int Width
+        {
+            get { return width; }
+            set { 
+                if(value < 0)
+                {
+                    throw(new Exception("width는 양수여야 합니다."));
+                }
+                width = value; 
+            }
+        }
+
+        private int height;
+
+        public int Height
+        {
+            get { return height; }
+            set {
+                if (value < 0)
+                {
+                    throw(new Exception("height는 양수여야 합니다."));
+                }
+                height = value;
+            }
+        }
+
+
+        public Box(int width, int height)
+        {
+            this.Width = width;
+            this.height = height;
+        }
+
+        public int GetArea()
+        {
+            return this.Width * this.Height;
+        }
+
+    }
+}
--- ClassAdv/Program.cs
+++ ClassAdv/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClassAdv
+namespace ClassAdv32
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            // 소멸자 예시
+            Product product1 = new Product("과자", 1000);
+            Product product2 = new Product("아이스크림", 1000);
+            Product product3 = new Product("케이크", 1000);
+
+            // 속성(Property) 실습
+            Box box = new Box(10, 10);
+            box.Width = 100;
+            box.Height = 100;
+            box.GetArea();
+            try
+            {
+                box.Width = -10;    // 잘못된 값이 들어간 경우
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
