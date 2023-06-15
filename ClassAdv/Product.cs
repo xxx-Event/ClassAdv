4f49ae8b48cc4c13ced500cd0312a0d8e89e1d64
xxx-Event <s2119@e-mirim.hs.kr>
2023-06-15 09:53:44 +0900

    소멸자 실습

---
+++ ClassAdv/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassAdv32
+{
+    internal class Product
+    {
+        private string name;
+        private int price;
+
+        public Product(string name, int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+        ~Product(){
+            Console.WriteLine(this.name + "의 소멸자 호출");
+        }
+    }
+}
